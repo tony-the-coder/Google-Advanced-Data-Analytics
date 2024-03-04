365192006539636a6be652eaad3df983bd606fbe
Tony Aldea <tonythecoder@outlook.com>
2024-03-03 19:30:53 -0500

    first commit

---
+++ Get Started With Python/Google Advanced Data Analytics/Get Started with Python from Google/test.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+
+# with open("waze_dataset.csv", "r") as file:
+#     df=file.read()
+
+# df.head()
+
+df = pd.read_csv('waze_dataset.csv')
+
+# df.head()
+print(df)
+
